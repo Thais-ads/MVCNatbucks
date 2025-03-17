17cfeb13f510dc61d0860a06a4da9772c55f6029
thais-ads <thaiss.spinoza@gmail.com>
2025-03-17 15:33:51 -0300

    fix:adjust codiguin

--- NathBucks/obj/Debug/net6.0/NathBucks.AssemblyInfo.cs
+++ NathBucks/obj/Debug/net6.0/NathBucks.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("NathBucks")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+767e2169c96fd82c42ba764683cf93f866114f26")]
 [assembly: System.Reflection.AssemblyProductAttribute("NathBucks")]
 [assembly: System.Reflection.AssemblyTitleAttribute("NathBucks")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
